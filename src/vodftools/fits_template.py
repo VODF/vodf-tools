21783795f8d8002f4b765f5de1dd90bddac9bc2f
Karl Kosack <karl.kosack@cea.fr>
2023-12-22 18:03:18 +0100

    code to convert to fits tpl

---
+++ src/vodftools/fits_template.py
@@ -0,0 +1,147 @@
+"""Defines the methods to generate a FITS Template file from a Schema
+
+This follows the Visitor pattern, where each class is decorated with a function
+@fits_template.generator(CLASS) that tells it how to convert that class into a
+fits template.
+
+The results can be printed as follows, assuming schema is an object from schema.py:
+
+.. code-block: python
+
+    print("\n".join(fits_template(schema)))
+
+"""
+import textwrap
+from pathlib import Path
+
+from astropy import units as u
+
+from typing import Generator
+from .schema import (
+    Column,
+    ColumnGroup,
+    Extension,
+    Header,
+    HeaderGroup,
+    SchemaElement,
+    Table,
+)
+from .visitor import visitor
+
+__all__ = ["fits_template", 'write_fits_template']
+
+
+@visitor
+def fits_template(schema: SchemaElement) -> Generator:
+    """
+    Function to create a generator for a FITS TPL from a given schema object.
+
+    Parameters
+    ----------
+    schema: SchemaElement
+        input schema
+
+    Returns
+    -------
+    Generator:
+        lines of the TPL file
+    """
+
+def write_fits_template(schema: SchemaElement, output_file: Path) -> None:
+    """Write a FITS tpl file for the given SchemaElement
+
+    Parameters
+    ----------
+    schema: SchemaElement
+        Input schema
+    output_file: str
+        Output filename or path
+    """
+    with Path(output_file).open(mode='w') as out:
+        for line in fits_template(schema):
+            out.write(line)
+            out.write("\n")
+
+
+
+
+
+#!/usr/bin/env python3
+@fits_template.generator(Header)
+def _(hdr):
+    extra = ""
+    if hdr.unit:
+        extra += f" [{u.Unit(hdr.unit).to_string('fits')}]"
+    if hdr.dtype:
+        extra += f" ({hdr.dtype.name})"
+    if len(hdr.key) <= 8:
+        yield f"{hdr.key.upper():8s} =       /{extra} {hdr.description:50s}"
+    else:
+        yield f"HIERARCH {hdr.key.upper()} = /{extra} {hdr.description:50s}"
+
+
+@fits_template.generator(HeaderGroup)
+def _(grp, **kwargs):
+    yield ""
+    yield "# ======================================="
+    yield f"# {grp.description}"
+    yield "# ======================================="
+
+    for header in grp.headers:
+        yield from fits_template(header)
+
+
+@fits_template.generator(Extension)
+def _(hdu):
+    yield "#" * 70
+    yield f"# HDU: {hdu.name}"
+    yield "# DESCRIPTION: "
+    yield from textwrap.wrap(
+        hdu.description,
+        initial_indent="#    ",
+        subsequent_indent="#    ",
+        drop_whitespace=True,
+    )
+    yield "#" * 70
+    yield "XTENSION = BINTABLE"
+    yield f"EXTNAME  = {hdu.name} / {hdu.description:.70s}"
+    for header in hdu.headers:
+        yield from fits_template(header)
+
+
+@fits_template.generator(Column)
+def _(col):
+    yield f"TTYPE# = {col.name} / {col.description:.70s}"
+    yield f"TFORM# = {col.dtype}"
+    if col.unit:
+        yield (
+            f"TUNIT# = {u.Unit(col.unit).to_string('fits')} "
+            f"/ or convertable to '{u.Unit(col.unit).physical_type}'"
+        )
+    if col.ucd:
+        yield f"TUCD#  = {col.ucd}"
+    yield ""  # spacer
+
+
+@fits_template.generator(ColumnGroup)
+def _(grp, **kwargs):
+    yield ""
+    yield "# ---------------------------------------"
+    yield f"# {grp.description}"
+    yield "# ---------------------------------------"
+
+    for column in grp.columns:
+        yield from fits_template(column)
+
+
+@fits_template.generator(Table)
+def _(table, **kwargs):
+    yield from fits_template.generators[Extension](
+        table
+    )  # bit if a hack to call parent
+    yield ""
+    yield "# ======================================="
+    yield "# Binary Table"
+    yield "# ======================================="
+    for column in table.columns:
+        yield from fits_template(column)
