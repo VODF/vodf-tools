46fa6cbabe19e51d7df8a44c18f1f1c0e79da690
Karl Kosack <karl.kosack@cea.fr>
2024-01-02 14:53:24 +0100

    add tests

--- src/vodftools/tests/test_vodf_metadata.py
+++ src/vodftools/tests/test_vodf_metadata.py
@@ -2,11 +2,15 @@
 import astropy.units as u
 from astropy.io import fits
 
-from vodftools.vodf_metadata import earth_location
+from vodftools.vodf_metadata import earth_location_headers
+from vodftools.fits_template import write_fits_template
 
 
-def test_earth_location():
+def test_earth_location_to_template():
+    write_fits_template(earth_location_headers, "earth.tpl")
 
+
+def test_earth_location_validate():
     # some test data to check
     header = fits.Header(
         [
--- src/vodftools/vodf_metadata.py
+++ src/vodftools/vodf_metadata.py
@@ -3,9 +3,9 @@
 Defines the metadata for VODF files using the metaschemas defined in schema.py
 """
 
-from .schema import HeaderGroup, Header
+from .schema import HeaderGroup, Header, Table
 
-earth_location = HeaderGroup(
+earth_location_headers = HeaderGroup(
     description="Earth Location Information",
     headers=[
         Header(key="GEOLON", description="Earth Longitude", unit="deg", required=False),
@@ -17,4 +17,9 @@ earth_location = HeaderGroup(
             required=False,
         ),
     ],
+)
+
+
+event_list = Table(
+    name="EVENTS", description="Event List", headers=[earth_location_headers,]
 )
