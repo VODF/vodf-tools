20e8e18a1350417dd37edb70ab3cb885a3760f2f
Karl Kosack <karl.kosack@cea.fr>
2024-03-12 16:44:16 +0100

    added some tests

---
+++ src/vodftools/tests/test_schema.py
@@ -0,0 +1,45 @@
+from vodftools.schema import Column, ColumnGroup
+from pydantic import ValidationError
+
+import pytest
+
+def test_columns_and_groups():
+    c1 = Column(
+        name="energy",
+        description="gamma ray energy (reconstructed)",
+        ucd="phys.energy;em.gamma",
+        unit="TeV",
+        dtype="float32",
+    )
+
+    c2 = Column(
+        name="time",
+        description="gamma ray arrival time",
+        ucd="time.epoch",
+        unit="s",
+        dtype="float64",
+    )
+
+    cols = ColumnGroup(columns=[c1,c2], description="A group of columns")
+
+    assert c1 in cols.columns
+
+    # should fail since ucd is not valid
+    with pytest.raises(ValidationError):
+        c3 = Column(
+            name="time",
+            description="gamma ray arrival time",
+            ucd="some-invalid-name",
+            unit="s",
+            dtype="float64",
+        )
+
+
+    # should fail since unit is not valid
+    with pytest.raises(ValidationError):
+        c3 = Column(
+            name="time",
+            description="gamma ray arrival time",
+            unit="invalid-unit",
+            dtype="float64",
+        )
