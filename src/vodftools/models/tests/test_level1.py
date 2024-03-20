de9d44383df3ec5d3e10f7f4a675e95cc5925cbf
Karl Kosack <karl.kosack@cea.fr>
2024-03-20 16:47:55 +0100

    better test naming in verbose output

--- src/vodftools/models/tests/test_level1.py
+++ src/vodftools/models/tests/test_level1.py
@@ -5,12 +5,13 @@ from vodftools.models.level1 import event_list_hdu, soi_hdu
 from vodftools.fits_template import fits_template
 
 
-@pytest.mark.parametrize("schema", [event_list_hdu, soi_hdu])
-def test_convert_classes(schema):
+@pytest.mark.parametrize(
+    "schema",
+    [
+        pytest.param(event_list_hdu, id="eventlist"),
+        pytest.param(soi_hdu, id="SOI table"),
+    ],
+)
+def test_convert_schemas(schema):
     for line in fits_template(schema):
-        print(line)
-
-
-def test_event_list():
-    for line in fits_template(event_list_hdu):
         print(line)
