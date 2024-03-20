73b58e97b0b00d3f67b2c1b671702781334388c2
Karl Kosack <karl.kosack@cea.fr>
2024-03-20 16:12:07 +0100

    better test

--- src/vodftools/models/tests/test_level1.py
+++ src/vodftools/models/tests/test_level1.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-from vodftools.models.level1 import event_list_hdu
+import pytest
+from vodftools.models.level1 import event_list_hdu, soi_hdu
 from vodftools.fits_template import fits_template
+
+
+@pytest.mark.parametrize("schema", [event_list_hdu, soi_hdu])
+def test_convert_classes(schema):
+    for line in fits_template(schema):
+        print(line)
+
 
 def test_event_list():
     for line in fits_template(event_list_hdu):
