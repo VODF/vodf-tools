ccbd47dcee1d91c1e5f53d66b36c0a30b7bf82d4
Karl Kosack <karl.kosack@cea.fr>
2025-01-14 17:31:33 +0100

    improve tests

--- src/vodftools/models/tests/test_level1.py
+++ src/vodftools/models/tests/test_level1.py
@@ -4,16 +4,24 @@
 import pytest
 from vodftools.fits_template import fits_template, write_fits_template
 from vodftools.models.level1 import event_file, event_list_hdu, soi_hdu
+from vodftools.plantuml import plantuml
 
 schemas_to_test = [
     pytest.param(event_list_hdu, id="event_list"),
     pytest.param(soi_hdu, id="soi_table"),
     pytest.param(event_file, id="event_file"),
+    pytest.param(event_file, id="eff_area_2d_hdu"),
+]
+
+converters = [
+    pytest.param(fits_template, id="fits"),
+    pytest.param(plantuml, id="plantuml"),
 ]
 
 
+@pytest.mark.parametrize("converter", converters)
 @pytest.mark.parametrize("schema", schemas_to_test)
-def test_convert_schemas(schema):
+def test_convert_schemas(schema, converter):
     """try to convert schema to a fits template"""
     for line in fits_template(schema):
         print(line)
